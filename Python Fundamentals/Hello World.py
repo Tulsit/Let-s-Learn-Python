79733df2112bea701953117183d72710b740a367
Tulsi Thakkar <81306817+Tulsit@users.noreply.github.com>
2021-08-09 14:16:33 +0530

    Delete Hello World.py

--- Python Fundamentals/Hello World.py
+++
@@ -1,9 +0,0 @@
-# Let-s-Learn-Python
-'''
-Topic 1 : Print Function :
---> This will print the text given in double quotes
-Syntax :
-        print("text to be displayed")
-'''
-
-print("Hello World")
