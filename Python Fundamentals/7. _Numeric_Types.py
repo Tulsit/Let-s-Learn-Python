f2a5c5758d6d2f0aff655d4fec99e27fd9c8b97e
Tulsi Thakkar <81306817+Tulsit@users.noreply.github.com>
2021-08-11 09:40:55 +0530

    Add files via upload

---
+++ Python Fundamentals/7. _Numeric_Types.py
@@ -0,0 +1,30 @@
+'''
+Topic 7 : Numeric Types in Python.
+--> There are 3 numeric types in python:
+    1. int : It is a whole number,positive or negative, without decimals, of unlimited length
+    2. float : It is a number, positive or negative, containing one or more decimals
+    3. Complex : They are written with a "j" as the imaginary part
+
+Syntax :
+        variable_name = number
+'''
+
+a = 9                   #int
+b = 12.26               #float
+c = 3j                  #complex
+d = -1123               #int
+e = 2.342               #float
+f = -22.1100456         #float
+g = 40e2                #float
+h = -85j                #complex
+i = 22j+11              #complex
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
