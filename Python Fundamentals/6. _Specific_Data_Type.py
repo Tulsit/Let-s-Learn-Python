b2ef39d2e0ec36d36f81f7c4ca3addf6b58ea9b5
Tulsi Thakkar <81306817+Tulsit@users.noreply.github.com>
2021-08-11 09:40:27 +0530

    Add files via upload

---
+++ Python Fundamentals/6. _Specific_Data_Type.py
@@ -0,0 +1,21 @@
+'''
+Topic 6 : Setting the Specific Data Type : If you want to specify the data type, you can use the following constructor functions:
+Syntax :
+        variable_name = data_type(value)
+        print(variable_name)
+'''
+a = int(3)
+print(a)
+print(type(a))
+
+b = str("Hello!")
+print(b)
+print(type(b))
+
+c = float(10.5)
+print(c)
+print(type(c))
+
+d = bool(False)
+print(d)
+print(type(d))
